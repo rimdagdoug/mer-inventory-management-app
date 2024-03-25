8528aacdbfbcd8f02b447a9a386809b7c23ebbb0
rim <rimdagdoug99@gmail.com>
2024-03-25 21:36:35 +0100

    create user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,44 @@
+const mongoose=require("mongoose");
+
+const userSchema=mongoose.Schema({
+    nema:{
+        type: String,
+        required: [true,"Please add a name"]
+    },
+    email:{
+        type: String,
+        required:[true,"Please add a email"],
+        unique: true,
+        trim: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid email"
+        ]
+    },
+    password:{
+        type: String,
+        required: [true,"Please add a password"],
+        minlength:[6,"Password must be up to 6 characters"],
+        maxlength:[23,"Password must not be more than to 23 characters "],
+    },
+    photo:{
+        type: String,
+        required: [true,"Please add a photo"],
+        default: "https://i.ibb.co/4pDNDK1/avatar.png" 
+    },
+    phone:{
+        type: String,
+        default: "+216" 
+    },
+    bio:{
+        type: String,
+        maxlength:[250,"Password must not be more than to 250 characters "],
+        default: "bio" 
+    }
+},{
+    timestamps: true,
+})
+
+
+const User = mongoose.model("User", userSchema);
+module.exports= User;
