8b2496a2ff8a30d31dede47e371872c7504b738d
rim <rimdagdoug99@gmail.com>
2024-03-25 01:17:26 +0100

    starter project

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config(); // Utilisation de require('dotenv').config()
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./config/connectDB.js");
+
+const app = express();
+
+connectDB();
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+//Routes
+ app.get("/", (req,res) => {
+     res.send("Home Page");
+     
+});
+
+// app.get('/', (req, res) => {
+//     res.status(200).json({
+        
+//     })
+// })
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
