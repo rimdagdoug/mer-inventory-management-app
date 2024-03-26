a7010be514eb7147bbe5151d153cc4f5c38e8d98
rim <rimdagdoug99@gmail.com>
2024-03-26 16:44:28 +0100

    Register new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,46 @@
+//évitezd'écrire explicitement un bloc try-catch 
+//dans chaque route qui utilise des opérations asynchrones
+const asyncHandler= require("express-async-handler");
+const User=require("../models/userModel");
 
-const registerUser =  (req, res) => {
-    if (!req.body.email) {
-        res.status(400);
-        throw new Error("Please add an email");
+const registerUser = asyncHandler( async (req, res) => {
+    const {name,email,password}= req.body
+
+    //validation
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error("please fill in all required fields")
     }
-    res.send("Register user");
-};
+    if(password.length <6){
+        res.status(400)
+        throw new Error("password must be up to 6 charcters")
+    }
+    //check if user email already exists
+     const userExists= await User.findOne({email})
+
+     if(userExists){
+        res.status(400)
+        throw new Error("Email has already been registered")
+     }
+
+     //Creat new user
+     const user = await User.create({
+        name,
+        email,
+        password
+     })
+ 
+     
+     if(user){
+        const {_id,name,email,photo,phone,bio}=user
+        res.status(201).json({
+            _id,name,email,photo,phone,bio
+        })
+     }else{
+        res.status(400)
+        throw new Error("Invalid user data")
+     }
+});
 
 
 module.exports = {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const userSchema=mongoose.Schema({
-    nema:{
+    name:{
         type: String,
         required: [true,"Please add a name"]
     },
