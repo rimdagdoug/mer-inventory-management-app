ccb3544de48f03f12592c9255f8dcd3ab1bc69ee
rim <rimdagdoug99@gmail.com>
2024-03-26 22:33:07 +0100

    Generate JSON Web Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,11 @@
 //dans chaque route qui utilise des opérations asynchrones
 const asyncHandler= require("express-async-handler");
 const User=require("../models/userModel");
+const jwt = require('jsonwebtoken');
+
+const  generatToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET,{expiresIn: "1d"})
+};
 
 
 const registerUser = asyncHandler( async (req, res) => {
@@ -24,20 +29,23 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new Error("Email has already been registered")
      }
 
-     
+    
 
      //Creat new user
      const user = await User.create({
         name,
         email,
         password,
-     })
+     });
+
+      //Generate token
+      const token=generatToken(user._id)
  
 
      if(user){
         const {_id,name,email,photo,phone,bio}=user
         res.status(201).json({
-            _id,name,email,photo,phone,bio
+            _id,name,email,photo,phone,bio,token,
         })
      }else{
         res.status(400)
