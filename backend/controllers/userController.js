7d23c88a6613df6ee2ae573b436268e93e3e9e7c
rim <rimdagdoug99@gmail.com>
2024-03-27 14:09:30 +0100

    Create Protect MiddleWare and UserProfile Controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -121,8 +121,24 @@ const logout = asyncHandler(async(req,res) => {
       return res.status(200).json({message: "Successfully logged out"});
 });
 
+//get user data
+const getUser= asyncHandler(async(req,res)=>{
+    const user = await User.findById(req.user._id);
+
+    if(user){
+        const {_id,name,email,photo,phone,bio}=user
+        res.status(200).json({
+            _id,name,email,photo,phone,bio,
+        })
+     }else{
+        res.status(400)
+        throw new Error("user not found");
+     }
+});
+
 module.exports = {
     registerUser,
     loginUser,
     logout,
+    getUser
 };
