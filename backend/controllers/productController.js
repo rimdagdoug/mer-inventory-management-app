d963bbfed82d0466071006784f865dddb4b21d93
rim <rimdagdoug99@gmail.com>
2024-03-29 00:46:39 +0100

    create product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,30 @@
 const asyncHandler = require("express-async-handler");
+const { Error } = require("mongoose");
+const Product = require("../models/productModel");
 
 const createProduct = asyncHandler(async(req,res) => {
-    res.send("product created");
+    const {name,sku,category,quantity,price,description}= req.body;
+
+    //validation
+    if (!name|| ! category || ! quantity || ! price || ! description) {
+        res.status(400)
+        throw new Error("please fill in all fields")
+    }
+    
+    //Manage image upload
+
+    // create product
+    const product = await Product.create({
+        user: req.user.id,
+        name,
+        sku,
+        category,
+        quantity,
+        price,
+        description,
+
+    })
+    res.status(201).json(product)
 });
 
 module.exports={
