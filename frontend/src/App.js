62fd53132dc2a5c9d48a5a283c2967f8bd1d457d
rim <rimdagdoug99@gmail.com>
2024-04-13 21:25:11 +0200

    create-auth-pages-and-routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Home from "./pages/Home/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
+import Reset from "./pages/auth/Reset";
+
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/register" element={<Register></Register>}></Route>
+      <Route path="/forgot" element={<Forgot></Forgot>}></Route>
+      <Route path="/resetpassword" element={<Reset></Reset>}></Route>
     </Routes>
    </BrowserRouter>
   );
---
+++ frontend/src/pages/auth/Reset.js
@@ -0,0 +1,12 @@
+
+
+
+import React from 'react'
+
+const Reset = () => {
+  return (
+    <div>Reset</div>
+  )
+}
+
+export default Reset
