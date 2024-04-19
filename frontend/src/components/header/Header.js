ec666db59152ca71c550d5c53d0028b9f688ddcf
rim <rimdagdoug99@gmail.com>
2024-04-19 16:23:23 +0200

    display-name-of-user

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,15 +1,19 @@
 
 import React from 'react'
 import { logoutUser } from '../../services/authServices';
-import { useDispatch } from "react-redux";
-import { SET_LOGIN } from '../../redux/features/auth/authSlice';
+import { useDispatch ,useSelector} from "react-redux";
+import { SET_LOGIN, selectName } from '../../redux/features/auth/authSlice';
 import { useNavigate} from 'react-router-dom';
+
+
+
 
 const Header = () => {
 
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const name = useSelector(selectName)
 
   const logout = async () => {
     await logoutUser();
@@ -22,7 +26,7 @@ const Header = () => {
           <div className="--flex-between">
             <h3>
               <span className="--fw-thin">Welcome, </span>
-              <span className="--color-danger">rim</span>
+              <span className="--color-danger">{name}</span>
             </h3>
             <button onClick={logout} className="--btn --btn-danger">
               Logout
