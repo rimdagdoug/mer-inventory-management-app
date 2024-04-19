ec0ee088d535f6195b91e14e2ddd84bf0baa48f7
rim <rimdagdoug99@gmail.com>
2024-04-19 20:57:39 +0200

    get-login-status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,11 +11,25 @@ import Layout from "./components/layout/Layout";
 import axios from "axios"
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getLoginStatus } from "./services/authServices";
+import { SET_LOGIN } from "./redux/features/auth/authSlice";
+
 
 axios.defaults.withCredentials  = true;
 
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    async function loginStatus() {
+      const status = await getLoginStatus()
+      dispatch(SET_LOGIN(status))
+    }
+    loginStatus();
+  }, [dispatch]) ;
+  
   return (
    <BrowserRouter>
    <ToastContainer />
--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -104,3 +104,19 @@ export const resetPassword = async (userData, resetToken) => {
       toast.error(message);
     }
   };
+
+
+  // get logi status
+export const getLoginStatus = async ( ) => {
+    try {
+      const response = await axios.get(
+        `${BACKEND_URL}/api/users/loggedin`, );
+      return response.data
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+      toast.error(message);
+    }
+  };
