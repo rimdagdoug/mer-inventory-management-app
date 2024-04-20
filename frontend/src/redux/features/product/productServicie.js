1b0e8fcab2b586ea85a1f7bc11ec482e38772909
rim <rimdagdoug99@gmail.com>
2024-04-20 22:37:40 +0200

    create-get-all-products-function-redux

--- frontend/src/redux/features/product/productServicie.js
+++ frontend/src/redux/features/product/productServicie.js
@@ -5,14 +5,22 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API_URL = `${BACKEND_URL}/api/products`
 
 //create new product
-export const createProduct = async (formData) => {
+const createProduct = async (formData) => {
     const response = await axios.post(API_URL,formData);
     return response.data
 };
 
+//Get all product
+const getProducts = async () => {
+    const response = await axios.get(API_URL);
+    return response.data
+};
+
+
 
 const productService = {
-    createProduct
+    createProduct,
+    getProducts,
 }
 
 export default productService
--- frontend/src/redux/features/product/productSlice.js
+++ frontend/src/redux/features/product/productSlice.js
@@ -29,6 +29,23 @@ export const createProduct = createAsyncThunk(
     }
 )
 
+//get all product
+export const getProducts = createAsyncThunk(
+    "products/getAll",
+    async (_, thunkAPI) => {
+        try {
+            return await productService.getProducts();
+        } catch (error) {
+            const message =
+            (error.response && error.response.data && error.response.data.message) ||
+            error.message ||
+            error.toString();
+            console.log(message)
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
 const productSlice = createSlice({
   name: "product",
   initialState,
@@ -45,6 +62,7 @@ const productSlice = createSlice({
     .addCase(createProduct.fulfilled,(state, action) => {
         state.isLoading= false;
         state.isSuccess = true;
+        state.isError= false;
         console.log(action.payload)
         state.products.push(action.payload);
         toast.success("Product added successfuly")
@@ -55,6 +73,22 @@ const productSlice = createSlice({
         state.message = action.payload;
         toast.error(action.payload);
       })
+      .addCase(getProducts.pending,(state) => {
+        state.isLoading = true 
+     })
+     .addCase(getProducts.fulfilled,(state, action) => {
+         state.isLoading= false;
+         state.isSuccess = true;
+         state.isError= false;
+         console.log(action.payload)
+         state.products = action.payload;
+     })
+     .addCase(getProducts.rejected, (state, action) => {
+         state.isLoading = false;
+         state.isError = true;
+         state.message = action.payload;
+         toast.error(action.payload);
+       });
   }
 });
 
