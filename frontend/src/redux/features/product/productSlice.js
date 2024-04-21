aa348a6dfd3b1a33f5fe1df120918f513cda20c9
rim <rimdagdoug99@gmail.com>
2024-04-21 15:39:29 +0200

    calculate-total-store-value

--- frontend/src/redux/features/product/productSlice.js
+++ frontend/src/redux/features/product/productSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import productService from './productServicie';
+import productService from './productServicie'; 
 import { toast } from "react-toastify";
 
 const initialState = {
@@ -9,27 +9,29 @@ const initialState = {
     isSuccess: false,
     isLoading: false,
     message: "",
+    totalStoreValue: 0,
+    outOfStock: 0,
+    category: "",
 };
 
-
-//create new product
+// Create new product
 export const createProduct = createAsyncThunk(
     "products/create",
     async (FormData, thunkAPI) => {
         try {
-            return await productService.createProduct(FormData)
+            return await productService.createProduct(FormData);
         } catch (error) {
             const message =
-            (error.response && error.response.data && error.response.data.message) ||
-            error.message ||
-            error.toString();
-            console.log(message)
-            return thunkAPI.rejectWithValue(message)
+                (error.response && error.response.data && error.response.data.message) ||
+                error.message ||
+                error.toString();
+            console.log(message);
+            return thunkAPI.rejectWithValue(message);
         }
     }
-)
+);
 
-//get all product
+// Get all products
 export const getProducts = createAsyncThunk(
     "products/getAll",
     async (_, thunkAPI) => {
@@ -37,63 +39,69 @@ export const getProducts = createAsyncThunk(
             return await productService.getProducts();
         } catch (error) {
             const message =
-            (error.response && error.response.data && error.response.data.message) ||
-            error.message ||
-            error.toString();
-            console.log(message)
-            return thunkAPI.rejectWithValue(message)
+                (error.response && error.response.data && error.response.data.message) ||
+                error.message ||
+                error.toString();
+            console.log(message);
+            return thunkAPI.rejectWithValue(message);
         }
     }
-)
+);
 
 const productSlice = createSlice({
-  name: "product",
-  initialState,
-  reducers: {
-    CALC_STORE_VALUE(state, action) {
-        console.log("store value")
-    }
-  },
-  extraReducers: (builder) => {
-    builder
-    .addCase(createProduct.pending,(state) => {
-       state.isLoading = true 
-    })
-    .addCase(createProduct.fulfilled,(state, action) => {
-        state.isLoading= false;
-        state.isSuccess = true;
-        state.isError= false;
-        console.log(action.payload)
-        state.products.push(action.payload);
-        toast.success("Product added successfuly")
-    })
-    .addCase(createProduct.rejected, (state, action) => {
-        state.isLoading = false;
-        state.isError = true;
-        state.message = action.payload;
-        toast.error(action.payload);
-      })
-      .addCase(getProducts.pending,(state) => {
-        state.isLoading = true 
-     })
-     .addCase(getProducts.fulfilled,(state, action) => {
-         state.isLoading= false;
-         state.isSuccess = true;
-         state.isError= false;
-         console.log(action.payload)
-         state.products = action.payload;
-     })
-     .addCase(getProducts.rejected, (state, action) => {
-         state.isLoading = false;
-         state.isError = true;
-         state.message = action.payload;
-         toast.error(action.payload);
-       });
-  }
+    name: "product",
+    initialState,
+    reducers: {
+        CALC_STORE_VALUE(state, action) {
+            const products = action.payload;
+            let totalValue = 0;
+            products.forEach((item) => {
+                const { price, quantity } = item;
+                const productValue = price * quantity;
+                totalValue += productValue;
+            });
+            state.totalStoreValue = totalValue;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createProduct.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(createProduct.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.isError = false;
+                state.products.push(action.payload);
+                toast.success("Product added successfully");
+            })
+            .addCase(createProduct.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+                toast.error(action.payload);
+            })
+            .addCase(getProducts.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getProducts.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.isError = false;
+                state.products = action.payload;
+            })
+            .addCase(getProducts.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+                toast.error(action.payload);
+            });
+    },
 });
 
-export const {CALC_STORE_VALUE} = productSlice.actions
+export const { CALC_STORE_VALUE } = productSlice.actions;
 
 export const selectIsLoading = (state) => state.product.isLoading;
+export const selectTotalStoreValue = (state) => state.product.totalStoreValue;
 
-export default productSlice.reducer
+export default productSlice.reducer;
