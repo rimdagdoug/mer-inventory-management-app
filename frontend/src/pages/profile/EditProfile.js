43862151e615ce4b28509f75680f794de715daa2
rim <rimdagdoug99@gmail.com>
2024-04-23 22:02:02 +0200

    edit-profile-part-2

--- frontend/src/pages/profile/EditProfile.js
+++ frontend/src/pages/profile/EditProfile.js
@@ -5,7 +5,8 @@ import Card from "../../components/card/Card";
 import Loader from "../../components/loader/Loader";
 import { selectUser } from "../../redux/features/auth/authSlice";
 import "./Profile.scss";
-
+import { toast } from "react-toastify";
+import { updateUser } from "../../services/authServices";
 
 const EditProfile = () => {
   const navigate = useNavigate();
@@ -40,7 +41,49 @@ const EditProfile = () => {
 
   const saveProfile = async (e) => {
     e.preventDefault();
-   
+    setIsLoading(true);
+    try {
+      // Handle Image upload
+      let imageURL;
+      if (
+        profileImage &&
+        (profileImage.type === "image/jpeg" ||
+          profileImage.type === "image/jpg" ||
+          profileImage.type === "image/png")
+      )
+       {
+        const image = new FormData();
+        image.append("file", profileImage);
+        image.append("cloud_name", "dumhrhfut");
+        image.append("upload_preset", "eicurjzb");
+
+        // First save image to cloudinary
+        const response = await fetch(
+          "https://api.cloudinary.com/v1_1/dumhrhfut/image/upload",
+          { method: "post", body: image }
+        );
+        const imgData = await response.json();
+        imageURL = imgData.url.toString();
+
+        // Save Profile
+        const formData = {
+          name: profile.name,
+          phone: profile.phone,
+          bio: profile.bio,
+          photo: profileImage ? imageURL : profile.photo,
+        };
+
+        const data = await updateUser(formData);
+        console.log(data);
+        toast.success("User updated");
+        navigate("/profile");
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      toast.error(error.message);
+    }
   };
 
   return (
@@ -97,7 +140,7 @@ const EditProfile = () => {
           </span>
         </form>
       </Card>
-      
+      <br />
     </div>
   );
 };
